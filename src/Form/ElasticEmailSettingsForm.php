305537833c0b77e02e2d949f2beb5bc5d3054204
mike.davis <mike.davis@2660157.no-reply.drupal.org>
2015-11-11 13:41:03 +0000

    Issue #2612378 by mike.davis: ElasticEmailSettingsForm - update the use of variable_get

--- src/Form/ElasticEmailSettingsForm.php
+++ src/Form/ElasticEmailSettingsForm.php
@@ -25,10 +25,13 @@ class ElasticEmailSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('elastic_email.settings');
-
-    foreach (Element::children($form) as $variable) {
-      $config->set($variable, $form_state->getValue($form[$variable]['#parents']));
-    }
+    $config->set('username', $form['credentials']['username']['#value']);
+    $config->set('api_key', $form['credentials']['api_key']['#value']);
+    $config->set('queue_enabled', $form['options']['queue_enabled']['#value']);
+    $config->set('log_success', $form['options']['log_success']['#value']);
+    $config->set('credit_low_threshold', $form['settings']['credit_low_threshold']['#value']);
+    $config->set('use_default_channel', $form['settings']['use_default_channel']['#value']);
+    $config->set('default_channel', $form['settings']['default_channel']['#value']);
     $config->save();
 
     if (method_exists($this, '_submitForm')) {
@@ -45,11 +48,11 @@ class ElasticEmailSettingsForm extends ConfigFormBase {
     return ['elastic_email.settings'];
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
     global $base_url;
-
-    // Add CSS to make the AJAX part of the form look a little better.
-    _elastic_email_add_admin_css();
 
     // Emails won't get sent if allow_url_fopen is disabled.
     if (ini_get('allow_url_fopen') != 1) {
@@ -62,33 +65,23 @@ class ElasticEmailSettingsForm extends ConfigFormBase {
       '#title' => t('API Credentials'),
     ];
 
-    // @FIXME
-    // // @FIXME
-    // // The correct configuration object could not be determined. You'll need to
-    // // rewrite this call manually.
-    // $form['credentials'][ELASTIC_EMAIL_USERNAME] = array(
-    //     '#type' => 'textfield',
-    //     '#size' => 48,
-    //     '#title' => t('API username'),
-    //     '#required' => TRUE,
-    //     '#default_value' => variable_get(ELASTIC_EMAIL_USERNAME, ''),
-    //     '#description' => t('This is typically your Elastic Email account email address.')
-    //   );
+    $form['credentials']['username'] = array(
+      '#type' => 'textfield',
+      '#size' => 48,
+      '#title' => t('API username'),
+      '#required' => TRUE,
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('username'),
+      '#description' => t('This is typically your Elastic Email account email address.'),
+    );
 
-
-    // @FIXME
-    // // @FIXME
-    // // The correct configuration object could not be determined. You'll need to
-    // // rewrite this call manually.
-    // $form['credentials'][ELASTIC_EMAIL_API_KEY] = array(
-    //     '#type' => 'textfield',
-    //     '#size' => 48,
-    //     '#title' => t('API Key'),
-    //     '#required' => TRUE,
-    //     '#default_value' => variable_get(ELASTIC_EMAIL_API_KEY, ''),
-    //     '#description' => t('The API Key format is typically <tt>xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</tt>.')
-    //   );
-
+    $form['credentials']['api_key'] = array(
+      '#type' => 'textfield',
+      '#size' => 48,
+      '#title' => t('API Key'),
+      '#required' => TRUE,
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('api_key'),
+      '#description' => t('The API Key format is typically <tt>xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</tt>.'),
+    );
 
     // DIV to hold the results of the AJAX test call.
     $form['credentials']['test']['elastic-email-test-wrapper'] = [
@@ -103,30 +96,20 @@ class ElasticEmailSettingsForm extends ConfigFormBase {
       '#title' => t('Options'),
     ];
 
-    // @FIXME
-    // // @FIXME
-    // // The correct configuration object could not be determined. You'll need to
-    // // rewrite this call manually.
-    // $form['options'][ELASTIC_EMAIL_QUEUE_ENABLED] = array(
-    //     '#type' => 'checkbox',
-    //     '#title' => t('Queue outgoing messages'),
-    //     '#description' => t('When checked, outgoing messages will be queued via Drupal core system queue, and delivered when the queue is emptied at cron time. When unchecked, messages are delivered immediately (synchronously). Note that synchronous delivery can cause delay in page execution time.') .
-    //       '<br /><br />' . t('If enabled, you can use the <a href="@link" target="_blank">Queue UI</a> to view the queue.', array('@link' => 'https://www.drupal.org/project/queue_ui')),
-    //     '#default_value' => variable_get(ELASTIC_EMAIL_QUEUE_ENABLED, FALSE)
-    //   );
+    $form['options']['queue_enabled'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Queue outgoing messages'),
+      '#description' => t('When checked, outgoing messages will be queued via Drupal core system queue, and delivered when the queue is emptied at cron time. When unchecked, messages are delivered immediately (synchronously). Note that synchronous delivery can cause delay in page execution time.') .
+        '<br /><br />' . t('If enabled, you can use the <a href="@link" target="_blank">Queue UI</a> to view the queue.', array('@link' => 'https://www.drupal.org/project/queue_ui')),
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('queue_enabled'),
+    );
 
-
-    // @FIXME
-    // // @FIXME
-    // // The correct configuration object could not be determined. You'll need to
-    // // rewrite this call manually.
-    // $form['options'][ELASTIC_EMAIL_LOG_SUCCESS] = array(
-    //     '#type' => 'checkbox',
-    //     '#title' => t('Log message delivery success'),
-    //     '#description' => t('When checked, a log message will also be generated for <em>successful</em> email delivery. Errors are always logged.'),
-    //     '#default_value' => variable_get(ELASTIC_EMAIL_LOG_SUCCESS, FALSE)
-    //   );
-
+    $form['options']['log_success'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Log message delivery success'),
+      '#description' => t('When checked, a log message will also be generated for <em>successful</em> email delivery. Errors are always logged.'),
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('log_success'),
+    );
 
     // Fieldset for other settings.
     $form['settings'] = [
@@ -134,32 +117,28 @@ class ElasticEmailSettingsForm extends ConfigFormBase {
       '#title' => t('Settings'),
     ];
 
-    $form['settings']['elastic_email_credit_low_threshold'] = [
+    $form['settings']['credit_low_threshold'] = [
       '#type' => 'textfield',
       '#size' => 8,
       '#title' => t('Low Credit Threshold (USD)'),
       '#description' => t('Sets the lower threshold limit value of when to warn admin users about a low credit limit.'),
-      '#default_value' => \Drupal::config('elastic_email.settings')->get('elastic_email_credit_low_threshold'),
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('credit_low_threshold'),
     ];
 
-    $form['settings']['elastic_email_use_default_channel'] = [
+    $form['settings']['use_default_channel'] = [
       '#type' => 'checkbox',
       '#title' => t('Use a Default Channel'),
       '#description' => t('If no default channel is set, then the default (set by Elastic Email) is the sending email address.<br />Setting a default channel will add this value to every email that is sent, meaning that you can more easily identify email that has come from each specific site within the reporting section.'),
-      '#default_value' => \Drupal::config('elastic_email.settings')->get('elastic_email_use_default_channel'),
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('use_default_channel'),
     ];
 
     $url = parse_url($base_url);
-    // @FIXME
-    // Could not extract the default value because it is either indeterminate, or
-    // not scalar. You'll need to provide a default value in
-    // config/install/elastic_email.settings.yml and config/schema/elastic_email.schema.yml.
-    $form['settings']['elastic_email_default_channel'] = [
+    $form['settings']['default_channel'] = [
       '#type' => 'textfield',
       '#size' => 48,
       '#maxlength' => 60,
       '#title' => t('Default Channel'),
-      '#default_value' => \Drupal::config('elastic_email.settings')->get('elastic_email_default_channel'),
+      '#default_value' => \Drupal::config('elastic_email.settings')->get('default_channel'),
       '#states' => [
         'visible' => [
           ':input[name="elastic_email_use_default_channel"]' => [
